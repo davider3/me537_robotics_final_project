5c92cb466c8f2589ece3414daad9d0cfb33af0ce
davider3 <dreinhardt389@gmail.com>
2023-11-21 19:36:51 -0700

    Firmata working

---
+++ Software/blink.py
@@ -0,0 +1,29 @@
+# %% Setup
+
+# if pyfirmata is not installed, install it
+try:
+    from pyfirmata import Arduino, util
+except:
+    import pip
+    pip.main(['install', 'pyfirmata'])
+
+# Fix the deprecated part of the pyfirmata library
+if not hasattr(inspect, 'getargspec'):
+    inspect.getargspec = inspect.getfullargspec
+
+# other imports
+import time
+import inspect
+
+# %%
+
+# CHECK: check that you are connected to the correct com port before each run
+controller = Arduino('COM11')
+
+on = True
+for _ in range(20):
+    controller.digital[13].write(on)
+    on = not on
+    time.sleep(.1)
+
+controller.exit()
